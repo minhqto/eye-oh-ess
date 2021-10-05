9fc3726b896febe7f3376500441daa2dcb6b84c3
Minh To <minhqto3@gmail.com>
2021-10-05 00:10:17 -0400

    first commit

---
+++ FirstUIProject1/FirstUIProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstUIProject1
+//
+//  Created by Minh To on 2021-10-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
