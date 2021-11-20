386c8ab88c5303d6d5342f0ce58e822023586d0d
Minh To <minhqto3@gmail.com>
2021-11-20 14:09:49 -0500

    adding webservices

---
+++ WebServices/WebServices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebServices
+//
+//  Created by Minh To on 2021-11-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
